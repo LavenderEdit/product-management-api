55e9d5ffd0f54b1ef362e085212e247de332dde6
LavenderEdit <gercermagden@gmail.com>
2025-12-30 20:03:09 -0500

    feat: mejorar configuración de CORS en el controlador de productos
    - Se añadió la configuración de CORS en el `ProductController` para permitir los métodos GET, POST, PUT y DELETE.
    - Se especificaron los encabezados permitidos (`allowedHeaders = "*"`) para mejorar la flexibilidad en las solicitudes.
    - Esta configuración asegura que solo los métodos especificados puedan interactuar con la API de productos, mejorando la seguridad y el control de acceso.

--- src/main/java/studios/tkoh/producto_manager/controller/ProductController.java
+++ src/main/java/studios/tkoh/producto_manager/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import studios.tkoh.producto_manager.dto.ProductExcelDTO;
@@ -23,7 +24,7 @@ import studios.tkoh.producto_manager.service.ProductService;
  */
 @RestController
 @RequestMapping("/api/kardex")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ProductController {
 
     @Autowired
