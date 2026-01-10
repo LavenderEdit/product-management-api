1db69c6d26f5975df3a4d48b914f741cd29d7f9f
LavenderEdit <gercermagden@gmail.com>
2026-01-09 20:41:24 -0500

    chore(config): configure default JVM timezone at startup

--- src/main/java/studios/tkoh/producto_manager/ProductoManagerApplication.java
+++ src/main/java/studios/tkoh/producto_manager/ProductoManagerApplication.java
@@ -1,13 +1,21 @@
 package studios.tkoh.producto_manager;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductoManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductoManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductoManagerApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+        System.out.println("Zona horaria configurada a: " + new java.util.Date());
+    }
 
 }
