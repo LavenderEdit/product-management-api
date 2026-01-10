b0d0673a95b05b0b9a1b77c12a7d31d039248f0d
LavenderEdit <gercermagden@gmail.com>
2026-01-09 19:42:51 -0500

    feat(dto): add PersonDTO for personnel data transfer
    Provide a minimal DTO to expose personnel name and role in
    read-only API responses.

---
+++ src/main/java/studios/tkoh/producto_manager/dto/PersonDTO.java
@@ -0,0 +1,20 @@
+package studios.tkoh.producto_manager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDTO {
+
+    private String name;
+    private String role;
+}
